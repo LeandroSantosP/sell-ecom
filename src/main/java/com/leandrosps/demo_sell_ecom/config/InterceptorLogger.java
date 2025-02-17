efd74c2110aac9b02c68acd4980d9a5a90dc84eb
LeandroSantosP <skp.Tridimensional@gmail.com>
2025-02-17 07:27:00 -0300

    add

---
+++ src/main/java/com/leandrosps/demo_sell_ecom/config/InterceptorLogger.java
@@ -0,0 +1,33 @@
+package com.leandrosps.demo_sell_ecom.config;
+
+import org.springframework.lang.Nullable;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class InterceptorLogger implements HandlerInterceptor {
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+            throws Exception {
+        // TODO Auto-generated method stub
+        return HandlerInterceptor.super.preHandle(request, response, handler);
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler,
+            @Nullable Exception ex) throws Exception {
+        // TODO Auto-generated method stub
+        HandlerInterceptor.super.afterCompletion(request, response, handler, ex);
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
+            @Nullable ModelAndView modelAndView) throws Exception {
+        // TODO Auto-generated method stub
+        HandlerInterceptor.super.postHandle(request, response, handler, modelAndView);
+    }
+
+}
