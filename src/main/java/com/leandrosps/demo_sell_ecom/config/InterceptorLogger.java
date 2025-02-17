ac5641a1cb235ebca192dd4b2efa920d460fbc0e
LeandroSantosP <skp.Tridimensional@gmail.com>
2025-02-17 07:39:48 -0300

    add all

--- src/main/java/com/leandrosps/demo_sell_ecom/config/InterceptorLogger.java
+++ src/main/java/com/leandrosps/demo_sell_ecom/config/InterceptorLogger.java
@@ -12,22 +12,20 @@ public class InterceptorLogger implements HandlerInterceptor {
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
             throws Exception {
-        // TODO Auto-generated method stub
-        return HandlerInterceptor.super.preHandle(request, response, handler);
-    }
-
-    @Override
-    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler,
-            @Nullable Exception ex) throws Exception {
-        // TODO Auto-generated method stub
-        HandlerInterceptor.super.afterCompletion(request, response, handler, ex);
+        System.out.println("preHandler: " + request.getRequestURI());
+        return true;
     }
 
     @Override
     public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
             @Nullable ModelAndView modelAndView) throws Exception {
-        // TODO Auto-generated method stub
-        HandlerInterceptor.super.postHandle(request, response, handler, modelAndView);
+        System.out.println("postHandle: " + request.getRequestURI());
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler,
+            @Nullable Exception ex) throws Exception {
+        System.out.println("afterCompletion: " + request.getRequestURI());
     }
 
 }
